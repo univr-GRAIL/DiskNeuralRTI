a56fe4eab8c9577839635517e0965807602db61a
Tinsae Gebrechristos Dulecha <64842989+tgdulecha@users.noreply.github.com>
2025-06-19 14:57:23 +0200

    Update params.py

--- modules/utils/params.py
+++ modules/utils/params.py
@@ -1,7 +1,7 @@
 comp_coeff = 9  # latent features, number of code
 batch_size = 64  # batch size
 num_workers = 8  # number of workers for concurrent data loading
-max_epochs = 1  # number of epochs
+max_epochs = 100  # number of epochs
 bit_feat = 8  # parameters used for feature encoding.  use default value
 alpha = 0.3  # alpha, weight,  values for distillation loss function.
 data_path = 'test_dataset/Dome' # MLIC input images
